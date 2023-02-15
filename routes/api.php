9f823d320abc6b1645d802b9de08eed9b2b2b907
Eberth <itec_19@hotmail.com>
2023-02-15 16:16:15 +0100

    feat: :art: AdminMiddleware
    Create a AdminMiddleware an established the rules to only admin have access to determined routes. Add the AdminMidleware to Kernel.php file.

--- routes/api.php
+++ routes/api.php
@@ -28,13 +28,47 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::post('/register', [AuthenticateController::class, 'register'])->name('register');
 Route::post('/login', [AuthenticateController::class, 'login']);
 
-Route::middleware('auth:api')->group(function(){    
+// Route::middleware('auth:api')->group(function(){    
+//     Route::post('/logout', [AuthenticateController::class, 'logout']);
+//     Route::resource('/consumptions', ConsumptionController::class);
+//     Route::resource('/forums', ForumController::class);
+//     Route::resource('/comments', CommentController::class);
+//     Route::resource('/informs', InformsController::class);
+//     Route::resource('/notifications', NotificationController::class);   
+//     Route::resource('/advices', AdviceController::class);
+    
+// });
+
+//Admin
+
+Route::middleware(['auth:api', 'isAdmin'])->group(function () {
     Route::post('/logout', [AuthenticateController::class, 'logout']);
     Route::resource('/consumptions', ConsumptionController::class);
     Route::resource('/forums', ForumController::class);
     Route::resource('/comments', CommentController::class);
     Route::resource('/informs', InformsController::class);
     Route::resource('/notifications', NotificationController::class);   
+    Route::resource('/advices', AdviceController::class);
+});
+
+// Route::prefix(['auth:api','admin'])->middleware('auth')->group(function() {
+//     Route::get('/admin', [AdminController::class, 'index']);
+
+// });
+
+Route::middleware('auth:api')->group(function(){    
+    Route::post('/logout', [AuthenticateController::class, 'logout']);
+    //consumptions ->
+    Route::resource('/consumptions', ConsumptionController::class);
+    //forums ->
+    Route::post('/forums', [ForumController::class, 'store']);
+    //comments ->
+    Route::post('/comments', [CommentController::class, 'store']);
+    //informs ->
+    Route::resource('/informs', InformsController::class);
+    //notifications ->
+    Route::resource('/notifications', NotificationController::class);   
+    //advices ->
     Route::resource('/advices', AdviceController::class);
     
 });
